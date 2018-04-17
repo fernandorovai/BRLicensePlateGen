d667db686dad60336274f2a3078df43125afdc72
Fernando Rodrigues Jr <fernandorovai@hotmail.com>
2018-04-17 12:11:16 -0300

    [PlateGen] Adding statistics to compare the number of characters
    - Set a flag showStatistics to False to hide the histogram
    - Calculates the histogram of each character based on generated plates

--- plateGenerator.py
+++ plateGenerator.py
@@ -4,29 +4,39 @@ import matplotlib.pyplot as plt
 import random
 import os
 import sys
+import collections
 
 class PlateGenerator:
-    def __init__(self, exportImage=False, exportBox=False, visualizePlates=True):
+    def __init__(self, exportImage=False, exportBox=False, showPlates=True, showStatistics=True):
         self.dataFolder      = 'data'
         self.letters         = ["A", "B", "C", "D", "E", "F", "G",
                                 "H", "I", "J", "K", "L", "M", "N",
                                 "O", "P", "Q", "R", "S", "T", "U",
                                 "V", "Y", "W", "X"]  # Add z
-        self.numbers         = [x for x in range (0, 9)]
-        self.bboxes          = []
-        self.nLetters        = 3
-        self.nNumbers        = 4
-        self.charPadding     = 0
-        self.initialWidth    = 30
-        self.initialHeight   = 55
-        self.widthRef        = 30
-        self.heightRef       = 55
 
-        self.exportImage     = exportImage
-        self.exportBox       = exportBox
-        self.visualizePlates = visualizePlates
-        self.plateSample     = os.path.join(self.dataFolder, 'plateSample01.jpg')
-        self.plateIm         = Image.open(self.plateSample)
+        self.statistics      = collections.OrderedDict([("A",0), ("B",0), ("C",0), ("D",0), ("E",0),
+                                                        ("F",0), ("G",0), ("H",0), ("I",0), ("J",0),
+                                                        ("K",0), ("L",0), ("M",0), ("N",0), ("O",0),
+                                                        ("P",0), ("Q",0), ("R",0), ("S",0), ("T",0),
+                                                        ("U",0), ("V",0), ("Y",0), ("W",0), ("X",0),
+                                                        ("0",0), ("1",0), ("2",0), ("3",0), ("4",0),
+                                                        ("5",0), ("6",0), ("7",0), ("8",0), ("9",0)])
+        self.numbers                = [x for x in range (0, 9)]
+        self.bboxes                 = []
+        self.nLetters               = 3
+        self.nNumbers               = 4
+        self.charPadding            = 0
+        self.initialWidth           = 30
+        self.initialHeight          = 55
+        self.widthRef               = 30
+        self.heightRef              = 55
+
+        self.exportImage            = exportImage
+        self.exportBox              = exportBox
+        self.showStatistics         = showStatistics
+        self.visualizePlates        = showPlates
+        self.plateSample            = os.path.join(self.dataFolder, 'plateSample01.jpg')
+        self.plateIm                = Image.open(self.plateSample)
 
         self.resetReferences()
 
@@ -47,6 +57,9 @@ class PlateGenerator:
 
             image.paste(char, (self.widthRef, self.heightRef), char)
             self.widthRef += charW + self.charPadding
+
+            #increment statistics
+            self.statistics[randomChar] +=1
         return image
 
     def generateBox(self, charW, charH, tag):
@@ -68,6 +81,9 @@ class PlateGenerator:
 
             image.paste(number, (self.widthRef, self.heightRef), number)
             self.widthRef += numberW + self.charPadding
+
+            #increment statistics
+            self.statistics[str(randomNum)] +=1
         return image
 
     def generateDash(self, image):
@@ -103,15 +119,24 @@ class PlateGenerator:
                 self.visualizePlate(numbersImg)
 
             self.resetReferences()
+
+        if self.showStatistics:
+            self.visualizeStatistics()
+
         return plateBoxes, plateImgs
 
+    def visualizeStatistics(self):
+        plt.figure()
+        plt.title("Characters Histogram")
+        plt.bar(self.statistics.keys(), self.statistics.values(), 1, color='g')
+        plt.show()
 
 if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         numOfPlates = int(sys.argv[1])
         if numOfPlates > 0:
-            plateGen = PlateGenerator()
+            plateGen = PlateGenerator(showPlates=False)
             plateBoxes, plateImgs = plateGen.generatePlates(numOfPlates=numOfPlates)
     else:
         print("You should specify the number of plates")

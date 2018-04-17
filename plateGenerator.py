8f6fec6a149350cebdc4f743750f8823c53d0dfd
Fernando Rodrigues Jr <fernandorovai@hotmail.com>
2018-04-16 22:21:36 -0300

    [PlateGen] Adding number of plates as script parameter
    - Declare the number of generated plates on calling the script.

--- plateGenerator.py
+++ plateGenerator.py
@@ -3,29 +3,30 @@ from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import random
 import os
+import sys
 
 class PlateGenerator:
-    def __init__(self, exportImage=False, exportBox=False):
-        self.dataFolder    = 'data'
-        self.letters       = ["A", "B", "C", "D", "E", "F", "G",
-                              "H", "I", "J", "K", "L", "M", "N",
-                              "O", "P", "Q", "R", "S", "T", "U",
-                              "V", "Y", "W", "X"]  # Add z
-        self.numbers       = [x for x in range (0, 9)]
-        self.bboxes        = []
-        self.nLetters      = 3
-        self.nNumbers      = 4
-        self.charPadding   = 0
-        self.initialWidth  = 30
-        self.initialHeight = 55
-        self.widthRef      = 30
-        self.heightRef     = 55
+    def __init__(self, exportImage=False, exportBox=False, visualizePlates=True):
+        self.dataFolder      = 'data'
+        self.letters         = ["A", "B", "C", "D", "E", "F", "G",
+                                "H", "I", "J", "K", "L", "M", "N",
+                                "O", "P", "Q", "R", "S", "T", "U",
+                                "V", "Y", "W", "X"]  # Add z
+        self.numbers         = [x for x in range (0, 9)]
+        self.bboxes          = []
+        self.nLetters        = 3
+        self.nNumbers        = 4
+        self.charPadding     = 0
+        self.initialWidth    = 30
+        self.initialHeight   = 55
+        self.widthRef        = 30
+        self.heightRef       = 55
 
-        self.exportImage   = exportImage
-        self.exportBox     = exportBox
-
-        self.plateSample   = os.path.join(self.dataFolder, 'plateSample03.jpg')
-        self.plateIm       = Image.open(self.plateSample)
+        self.exportImage     = exportImage
+        self.exportBox       = exportBox
+        self.visualizePlates = visualizePlates
+        self.plateSample     = os.path.join(self.dataFolder, 'plateSample01.jpg')
+        self.plateIm         = Image.open(self.plateSample)
 
         self.resetReferences()
 
@@ -98,13 +99,22 @@ class PlateGenerator:
             plateImgs.append(numbersImg)
             plateBoxes.append(self.bboxes)
 
-            self.visualizePlate(numbersImg)
+            if self.visualizePlates:
+                self.visualizePlate(numbersImg)
+
             self.resetReferences()
         return plateBoxes, plateImgs
 
-plateGen = PlateGenerator()
-plateBoxes, plateImgs = plateGen.generatePlates(numOfPlates=2)
 
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        numOfPlates = int(sys.argv[1])
+        if numOfPlates > 0:
+            plateGen = PlateGenerator()
+            plateBoxes, plateImgs = plateGen.generatePlates(numOfPlates=numOfPlates)
+    else:
+        print("You should specify the number of plates")
 
 
 
